573bce9dc1defedaf3d9e033378663e3e3e00707
Ubuntu <tsalufe@gmail.com>
2014-07-24 03:18:58 +0000

    delete old examples

--- examples/Gehwah-HtmlClasses-test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'Gehwah.php';
-$cl=new HtmlClasses(file_get_contents('loudpicks.com.account.html'));
-file_put_contents('a.css',Gehwah::ProcessClasses($cl->run()));
-?>
--- examples/HtmlClasses-test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'Gehwah.php';
-$cl=new HtmlClasses(file_get_contents('loudpicks.com.account.html'));
-print_r($cl->run());
-?>
